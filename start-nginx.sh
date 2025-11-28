6ea5aa93285cc104b19ae207f4f7576caf7a53db
Franklin David Canduri Presilla <fcandurin914@e.uneg.edu.ve>
2025-11-28 11:48:54 -0800

    fix: Improve Nginx startup script with defaults and logging

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+# Set the port to the value of the PORT environment variable, or 8080 if it's not set.
+export PORT="${PORT:-8080}"
+
 # Use envsubst to replace the port in the nginx config template
-envsubst '
-${PORT}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${PORT}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
+
+# Print out the generated config file for debugging
+echo "--- Generated Nginx Config ---"
+cat /etc/nginx/conf.d/default.conf
+echo "------------------------------"
 
 # Start Nginx in the foreground
 exec nginx -g 'daemon off;'
