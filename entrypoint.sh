119cc5b36a60de9b4da1b04ead3e8b635a7bdde4
Franklin David Canduri Presilla <fcandurin914@e.uneg.edu.ve>
2025-11-29 19:17:40 -0800

    fix: install gettext for envsubst and simplify entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-# entrypoint.sh
+set -e
 
-# Substitute environment variables in the nginx configuration
-envsubst '$PORT' < /etc/nginx/nginx.template > /etc/nginx/conf.d/default.conf
+# This script will substitute environment variables in the nginx config template
+# and then start the nginx server.
 
-# Execute the command passed to this script
-exec "$@"
+# The PORT variable is passed by Cloud Run. Default to 8080 if not set.
+export PORT=${PORT:-8080}
+
+# Substitute the variable and create the final config file.
+envsubst < /etc/nginx/nginx.template > /etc/nginx/conf.d/default.conf
+
+# Start nginx in the foreground
+exec nginx -g 'daemon off;'
